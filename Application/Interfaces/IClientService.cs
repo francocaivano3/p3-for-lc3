d8026a73aeefd1c375616336854cb862b632bc0a
Franco Berlochi <francoberlochi@gmail.com>
2024-10-19 20:15:46 -0300

    interface y servicio de cliente

---
+++ Application/Interfaces/IClientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IClientService
+    {
+        void Register();
+        string Login();
+        List<Event> GetAll();
+        List<Ticket> Tickets();
+    }
+}
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +10,21 @@ namespace Domain.Entities
 {
     public abstract class User
     {
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
         protected string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
         protected string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(64)")]
         protected string Password { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
         protected string Phone {  get; set; }
+
+        [Required]
         public string Role {  get; set; }
         protected User(string name, string email, string password, string phone, string role)
         {
