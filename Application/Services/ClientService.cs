05679db68d7538d294102f1b6b7ed3cec7886b0f
Franco Berlochi <87444239+FrancoBerlochi@users.noreply.github.com>
2024-10-23 13:17:48 -0300

    Update ClientService.cs

--- Application/Services/ClientService.cs
+++ Application/Services/ClientService.cs
@@ -1,9 +1,5 @@
 ﻿using Application.Interfaces;
 using Domain.Entities;
-<<<<<<< HEAD
-using Domain.Interfaces;
-=======
->>>>>>> e05bf1e512497b1cf92af63a84aa7bfc0628d802
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,61 +10,25 @@ namespace Application.Services
 {
     public class ClientService : IClientService
     {
-<<<<<<< HEAD
-        private readonly IClientRepository _clientRepository;
-        private readonly IEventRepository _eventRepository;
-        public ClientService(IClientRepository clientRepository, IEventRepository eventRepository)
+        public void Register() 
         {
-            _clientRepository = clientRepository;
-            _eventRepository = eventRepository;
+            // Lógica para el registro
         }
 
-        public bool BuyTicket(int clientId, int eventId)
+        public string Login() 
         {
-            Client client = _clientRepository.GetClientById(clientId);
-            if (client == null) 
-            {
-                return false;
-            }
-
-            Event eventEntity = _eventRepository.GetById(eventId);
-            if (eventEntity == null)
-            {
-                return false;
-            }
-
-            Ticket availableTicket = eventEntity.Tickets.FirstOrDefault(t => t.State == true);
-            if (availableTicket == null) 
-            {
-                return false;
-            }
-
-            availableTicket.ClientId = clientId;
-            availableTicket.State = false;
-
-            client.MyTickets.Add(availableTicket);
-
-            _clientRepository.UpdateClient(client);
-            _clientRepository.BuyTicket(availableTicket);
-
-            return true;
+            return "hoa"; // Lógica para el inicio de sesión
         }
 
-        public List<Ticket> GetAllMyTickets(int clientId)
-        {
-            return _clientRepository.GetAllMyTickets(clientId);
+        public List<Event> GetAll() 
+        { 
+            var a = new List<Event>();
+            return a; // Devuelve una lista de eventos
         }
 
-        public Client GetClientById(int clientId)
-        {
-            return _clientRepository.GetClientById(clientId);
+        public List<Ticket> Tickets() 
+        { 
+            return new List<Ticket>(); // Devuelve una lista de tickets
         }
-=======
-        public void Register() { }
-        public string Login() { return "hoa"; }
-        public List<Event> GetAll() { var a = new List<Event>();
-                return a ; }
-        public List<Ticket> Tickets() { return new List<Ticket>(); }
->>>>>>> e05bf1e512497b1cf92af63a84aa7bfc0628d802
     }
 }
