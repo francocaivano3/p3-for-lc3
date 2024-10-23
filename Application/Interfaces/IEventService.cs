1bc84fe1568af3dcd3b01525ea63cac9d0ab2cde
Franco Berlochi <87444239+FrancoBerlochi@users.noreply.github.com>
2024-10-23 13:15:22 -0300

    Update IEventService.cs

--- Application/Interfaces/IEventService.cs
+++ Application/Interfaces/IEventService.cs
@@ -9,11 +9,9 @@ namespace Application.Interfaces
 {
     public interface IEventService
     {
-<<<<<<< HEAD
-        void CreateEvent(string name, string address, string city, DateTime date, int numberOfTickets, string category, float price, int eventOrganizerId);
-=======
+
         void CreateEvent(string name, string address, string city, DateTime date, string category, float price, EventOrganizer eventOrganizer);
->>>>>>> e05bf1e512497b1cf92af63a84aa7bfc0628d802
+
         Event GetEventById(int eventId);
         List<Event> GetAllEvents();
         List<Event> GetEventsByOrganizerId(int organizerId);
