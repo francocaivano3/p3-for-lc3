d1e9d96adf0a167751bfa34be3ed51c2f301e6fd
Franco Berlochi <87444239+FrancoBerlochi@users.noreply.github.com>
2024-10-23 13:13:53 -0300

    Update IClientService.cs

--- Application/Interfaces/IClientService.cs
+++ Application/Interfaces/IClientService.cs
@@ -9,13 +9,9 @@ namespace Application.Interfaces
 {
     public interface IClientService
     {
-<<<<<<< HEAD
-        List<Ticket> GetAllMyTickets(int clientId);
-        bool BuyTicket(int clientId, int eventId);
-        Client GetClientById(int clientId);
-=======
+
         List<Event> GetAll();
         List<Ticket> Tickets();
->>>>>>> e05bf1e512497b1cf92af63a84aa7bfc0628d802
+
     }
 }
